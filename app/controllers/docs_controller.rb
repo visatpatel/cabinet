134f7ffcbfb9871374cc54498994bf7d1cbe639b
Visat Patel <visatpateler@gmail.com>
2019-03-05 07:25:46 -0500

    add user table, change index to show current user docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,7 +2,7 @@ class DocsController < ApplicationController
 	before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@docs = Doc.all.order("created_at DESC")
+		@docs = Doc.where(user_id: current_user)
 	end
 
 	def show
@@ -10,11 +10,11 @@ class DocsController < ApplicationController
 	end
 
 	def new
-		@doc = Doc.new
+		@doc = current_user.docs.build
 	end
 
 	def create
-		@doc = Doc.new(doc_params)
+		@doc = current_user.docs.build(doc_params)
 
 		if @doc.save
 			redirect_to @doc
